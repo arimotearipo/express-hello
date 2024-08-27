84ea0debd808f115f8c01405e9f1f16d4571daeb
adzahari <wnadzahari@ismarturus.com>
2024-08-27 14:28:25 +0800

    .

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (_, res) => {
-  console.log("received GET request")
+  console.log("received GET request...")
   res.json({message: 'Hello World!'}).status(200)
 })
 
